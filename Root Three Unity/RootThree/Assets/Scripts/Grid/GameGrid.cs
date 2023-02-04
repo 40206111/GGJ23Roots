2409593536f7b2dd9a0a683949b13f9cb31f308a
Timothy Sanderson <t.s.sanderson@hotmail.co.uk>
2023-02-04 23:31:00 +0000

    Bounding walls

--- Root Three Unity/RootThree/Assets/Scripts/Grid/GameGrid.cs
+++ Root Three Unity/RootThree/Assets/Scripts/Grid/GameGrid.cs
@@ -17,6 +17,11 @@ public class GameGrid : MonoBehaviour
 
     CameraResize CameraControl;
 
+    [SerializeField] Transform PlaneFor;
+    [SerializeField] Transform PlaneBack;
+    [SerializeField] Transform PlaneRight;
+    [SerializeField] Transform PlaneLeft;
+
     private void Start()
     {
         CameraControl = Camera.main.GetComponent<CameraResize>();
@@ -40,9 +45,9 @@ public class GameGrid : MonoBehaviour
     public void GenerateGrid()
     {
         ClearGrid();
-        for (int y = 0; y < Width; y++)
+        for (int y = 0; y < Height; y++)
         {
-            for (int x = 0; x < Height; x++)
+            for (int x = 0; x < Width; x++)
             {
                 Vector3 pos = new(x , 0, y);
                 int matInt = Random.Range(0, Colours.Count);
@@ -55,6 +60,25 @@ public class GameGrid : MonoBehaviour
 
         CameraControl?.ResizeCamera(Width, Height);
         GameManager.Instance.GridReadyForPlayer();
+
+        float unitsPerScale = 10f;
+        Vector3 scaleOnX = new Vector3(Width / unitsPerScale + 1f, 1f, 1f);
+        Vector3 scaleOnZ = new Vector3(1f, 1f, Height / unitsPerScale + 1f);
+        float yPos = unitsPerScale / 2f - 1;
+        Vector3 forPos = new Vector3(Width / 2f - 0.5f, yPos, Height - 0.5f);
+        Vector3 backPos = new Vector3(Width / 2f - 0.5f, yPos, -0.5f);
+        Vector3 rightPos = new Vector3(Width - 0.5f, yPos, Height / 2f - 0.5f);
+        Vector3 leftPos = new Vector3(-0.5f, yPos, Height / 2f - 0.5f);
+
+        ConfigurePlane(PlaneFor, forPos, scaleOnX);
+        ConfigurePlane(PlaneBack, backPos, scaleOnX);
+        ConfigurePlane(PlaneRight, rightPos, scaleOnZ);
+        ConfigurePlane(PlaneLeft, leftPos, scaleOnZ);
     }
 
+    void ConfigurePlane(Transform plane, Vector3 pos, Vector3 scale)
+    {
+        plane.position = pos;
+        plane.localScale = scale;
+    }
 }
